2155ad75b6cf63f0b60bd84151be4bfaba3b9197
Anuj Nirmal <anujrajeshnirmal@gmail.com>
2021-10-09 20:53:57 +0530

    Fixed the looping issue and added the neccesary filesand also made the footer with complete dynamic codes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header.js";
+import Footer from "./components/Footer.js"
+
+function App(){
+    return (
+        <div>
+            <Header />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
